21e8c808b031688f64a776e9c796e2c0a978596d
Boris Ostojic <sergejspam@gmail.com>
2014-01-16 14:56:18 +0100

    Create IO page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,34 @@
-console.log('This would be the main JS file.');
+(function ($) {
+  $(function () {
+    $("#alert").click(function () {
+      $.alert("This is simple alert message", "Title");
+    });
+
+    $("#prompt").click(function () {
+      $.prompt("Enter your name", "John")
+        .done(function (name) {
+          $.alert("Hi " + name, "Greeting");
+        })
+        .fail(function () {
+          $.alert("You didn't want to input your name", "See you next time");
+        });
+    });
+
+    $("#confirm").click(function () {
+      $.confirm("Confirm this action", "Please make choice")
+        .done(function () {
+          $.alert("You said OK", "Yupiii");
+        })
+        .fail(function () {
+          $.alert("You canceled!", "Noooo");
+        })
+    });
+
+    $("#actions button").click(function (events) {
+      $("#scripts pre").hide();
+      $("#code-for-" + events.target.id).show();
+    });
+
+    $("#scripts pre").hide();
+  });
+}(jQuery));
